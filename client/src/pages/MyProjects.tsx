1ffc04465927e04ab4030459fa9f15fa014a7be4
Yeasin Munshi <142327064+yeasin5060@users.noreply.github.com>
2026-05-01 18:32:47 +0600

    complete the pages myproject page

--- client/src/pages/MyProjects.tsx
+++ client/src/pages/MyProjects.tsx
@@ -1,10 +1,87 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import type { Project } from '../types';
+import { Loader2Icon, PlusIcon, TrashIcon } from 'lucide-react';
+import { useNavigate } from 'react-router-dom';
+import { dummyProjects } from '../assets/assets';
 
 const MyProjects = () => {
+  const [loding , setLoding] = useState(true);
+  const [projects , setProjects] = useState <Project[]>([]);
+  const navigate = useNavigate();
+
+  const fetchProjects = async () => {
+    setProjects(dummyProjects);
+      //simulate api call
+    setTimeout (()=> {
+      setLoding(false)
+    },1000)
+  }
+
+  const deleteProject = async (projectId : string)=> {
+
+  }
+  useEffect(()=> {
+    fetchProjects();
+  },[]);
+
   return (
-    <div>
-        <h1>My projects</h1>
-    </div>
+    <>
+      <div className='px-4 md:px-16 lg:px-24 xl:px-32'>
+        {loding ? (
+          <div className='flex items-center justify-center h-[80vh]'>
+            <Loader2Icon className='animate-spin text-indigo-200 size-7'/>
+          </div>
+        ) : projects.length > 0 ? (
+          <div className='py-10 min-h-[80vh]'>
+            <div className='flex items-center justify-between mb-12'>
+              <h1 className='text-2xl font-medium text-white capitalize'>my projects</h1>
+              <button className='flex items-center gap-2 text-white px-3 sm:px-6 py-1 sm:py-2 rounded bg-linear-to-br from-indigo-500 to-indigo-600 hover:opacity-90 active:scale-95 transition-all capitalize' onClick={()=> navigate('/')}><PlusIcon size={18}/> create new</button>
+            </div>
+            <div className='flex flex-wrap gap-3.5'>
+              {projects?.map((project)=>(
+                <div onClick={()=> navigate(`/projects/${project.id}`)} className='relative group w-72 max-sm:mx-auto cursor-pointer bg-gray-900/60 border border-gray-700 rounded-lg overflow-hidden shadow-md group hover:shadow-indigo-700/30 hover:border-indigo-800/80 transition-all duration-300' key={project.id}>
+                    {/*Desktop-like mini preview*/}
+                    <div className='relative w-full h-40 bg-gray-900 overflow-hidden border-b border-gray-800'>
+                      {project.current_code ? (
+                        <iframe className='absolute top-0 left-0 w-[1200px] h-[800px] origin-top-left pointer-events-none' srcDoc={project.current_code} sandbox='allow-scripts allow-same-origin' style={{transform:'scale(0.25)'}}/>
+                      ) : (
+                        <div className='flex items-center justify-center h-full text-gray-500'>
+                          <p>no preview</p>
+                        </div>
+                      )}
+                    </div>
+                    {/*content*/}
+                    <div className='p-4 text-white bg-linear-180 from-transparent group-hover:from-indigo-950 to-transparent transition-colors'>
+                      <div className='flex items-start justify-between'>
+                        <h2 className='text-lg font-medium line-clamp-2'>{project.name}</h2>
+                        <button className='px-2.5 py-0.5 mt-1 ml-2 text-xs bg-gray-800 border border-gray-700 rounded-full capitalize'>website</button>
+                      </div>
+                      <p className='text-gray-400 text-sm mt-1 line-clamp-2'>{project.initial_prompt}</p>
+                      <div className='flex items-center justify-between mt-6' onClick={(e)=> e.stopPropagation()}>
+                        <span className='text-xs text-gray-500'>{new Date(project.createdAt).toLocaleDateString()}</span>
+                        <div className='flex gap-3 text-white text-sm'>
+                          <button className='px-3 py-1.5 bg-white/10 hover:bg-white/15 rounded-md transition-all capitalize' onClick={()=> navigate(`/preview/${project.id}`)}>preview</button>
+                          <button className='px-3 py-1.5 bg-white/10 hover:bg-white/15 rounded-md transition-colors capitalize' onClick={()=> navigate(`/projects/${project.id}`)}>open</button>
+                        </div>
+                      </div>
+                    </div>
+                    <div onClick={ e => e.stopPropagation()}>
+                      <TrashIcon onClick={()=> deleteProject(project.id)} className='absolute top-3 right-3 scale-0 group-hover:scale-100 bg-white py-1.5 size-7 rounded text-red-500 text-xl cursor-pointer transition-all'/>
+                    </div>
+                </div>
+              ))}
+            </div>  
+          </div>
+        ): (
+          <div className='flex flex-col items-center justify-center h-[80vh]'>
+            <h1 className='text-3xl font-semibold text-gray-300'> You have no project yet</h1>
+            <button className='text-white px-5 py-2 mt-5 rounded-md bg-indigo-600 hover:bg-indigo-600 active:scale-95 transition-all capitalize' onClick={()=> navigate('/')}>
+              create new
+            </button>
+          </div>
+        )}
+      </div>
+    </>
   )
 }
 
