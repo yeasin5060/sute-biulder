581f6214e038d9c2c12212afe792f493c83cc504
Yeasin Munshi <142327064+yeasin5060@users.noreply.github.com>
2026-05-01 13:34:28 +0600

    add the all pages route in app page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,28 @@
-
+import React from "react"
+import { Route, Routes } from "react-router-dom"
+import Home from "./pages/Home"
+import Pricing from "./pages/Pricing"
+import Projects from "./pages/Projects"
+import MyProjects from "./pages/MyProjects"
+import Preview from "./pages/Preview"
+import Community from "./pages/Community"
+import View from "./pages/View"
 
 function App() {
 
   return (
-    <>
-      <h1 className="text-red-300">hello</h1>
-    </>
+    <div>
+      <Routes>
+        <Route path="/" element = {<Home/>}/>
+        <Route path="/pricing" element = {<Pricing/>}/>
+        <Route path="/projects/:projectId" element = {<Projects/>}/>
+        <Route path="/projects" element = {<MyProjects/>}/>
+        <Route path="/preview/:projectId" element = {<Preview/>}/>
+        <Route path="/preview/:projectId/:versionId" element = {<Preview/>}/>
+        <Route path="/community" element = {<Community/>}/>
+        <Route path="/view/:projectId" element = {<View/>}/>
+      </Routes>
+    </div>
   )
 }
 
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Home = () => {
   return (
     <div>
-      <h1>home</h1>
+      <h1 className='text-white'>home</h1>
     </div>
   )
 }
