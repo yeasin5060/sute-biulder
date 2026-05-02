ffab5c7a8ec75908d71ab8fec41dc9724f03b567
Yeasin Munshi <142327064+yeasin5060@users.noreply.github.com>
2026-05-02 14:24:34 +0600

    complete the left section in project page

--- client/src/pages/Projects.tsx
+++ client/src/pages/Projects.tsx
@@ -1,12 +1,12 @@
 import React, { useEffect, useState } from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
-import { Project } from '../types';
-import { Loader2Icon } from 'lucide-react';
+import { Loader2Icon, MessageSquareIcon, XIcon } from 'lucide-react';
+import type { Project } from '../types';
+import { dummyConversations, dummyProjects } from '../assets/assets';
 
 const Projects = () => {
-  const {ProjectId} = useParams();
+  const {projectId} = useParams();
   const navigate = useNavigate();
-
   const [project , setProject ] = useState<Project | null>(null);
   const [loding , setLoding] = useState(true);
   const [isGenerating , setIsGenerating] = useState(true);
@@ -15,7 +15,14 @@ const Projects = () => {
   const [isSaving , setIsSaving] = useState(false);
 
   const fetchProject = async () => {
-    
+    const project = dummyProjects.find(project => project.id === projectId);
+     setTimeout (()=> {
+      if(project){
+        setProject({...project , conversation : dummyConversations});
+        setLoding(false);
+        setIsGenerating(project.current_code ? false : true);
+      }
+    },2000)
   }
 
   useEffect(()=> {
@@ -29,10 +36,33 @@ const Projects = () => {
           <Loader2Icon className='size-7 animate-spin text-violet-200'/>
         </div>
       </>
-    )
+    ) 
   }
-  return (
-    <div>Projects</div>
+  return  project ? (
+    <div className='flex flex-col text-white bg-gray-900 h-screen w-full'>
+      {/* builder navbar */}
+      <div className='flex max-sm:flex-col sm:items-center gap-4 px-4 py-2 no-scrollbar'>
+        {/* left */}
+        <div className='flex items-center gap-2 sm:max-w-90 text-nowrap'>
+          <img className='h-6 cursor-pointer' onClick={()=> navigate('/')} src="/favicon.svg" alt="logo" />
+          <div className='max-w-64 sm:max-w-xs'>
+            <p className='text-sm font-medium capitalize truncate'>{project.name}</p>
+            <p className='text-xs text-gray-400 -mt-0.5'>Previewing last saved version</p>
+          </div>
+          <div className='sm:hidden flex flex-1 justify-end'>
+            {isMenuOpen ? <MessageSquareIcon onClick={()=> setIsMenuOpen(false)} className='size-6 cursor-pointer'/> : <XIcon onClick={()=> setIsMenuOpen(true)} className='size-6 cursor-pointer'/>}
+          </div>
+        </div>
+        {/* middle */}
+        <div></div>
+        {/* right */}
+        <div></div>
+      </div>
+    </div>
+  ) : (
+    <div className='flex items-center justify-center h-screen'>
+      <p className='text-2xl font-medium text-gray-200'>Unable to load project!</p>
+    </div>
   )
 }
 
