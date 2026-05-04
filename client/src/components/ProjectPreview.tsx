f8e8a2a46a7f38146c63cbcee306ad5b5b756623
Yeasin Munshi <142327064+yeasin5060@users.noreply.github.com>
2026-05-04 14:39:32 +0600

    project page downloadcode function complete

--- client/src/components/ProjectPreview.tsx
+++ client/src/components/ProjectPreview.tsx
@@ -1,4 +1,4 @@
-import React, { forwardRef, useEffect, useRef, useState } from 'react'
+import React, { forwardRef, useEffect, useImperativeHandle, useRef, useState } from 'react'
 import type { Project } from '../types';
 import { iframeScript } from '../assets/assets';
 import EditorPanel from './EditorPanel';
@@ -26,7 +26,29 @@ const ProjectPreview = forwardRef<ProjectPreviewRef,ProjectPreviewProps> (({proj
         desktop : 'w-full',
     }
 
-    console.log(selectedElement)
+    useImperativeHandle(ref , ()=> ({
+        getCode : ()=> {
+            const doc = iframeRef.current?.contentDocument;
+            if(!doc)return undefined;
+            //1. remove your selection class / attribites /outline from all elements
+            doc.querySelectorAll('.ai-selected-element,[data-ai-selected]').forEach((el)=>{
+                el.classList.remove('ai-selected-element');
+                el.removeAttribute('data-ai-selected');
+                (el as HTMLElement).style.outline = ''
+            });
+
+            //2. remove injected + script form the document
+            const previewStyle = doc.getElementById('ai-preview-style');
+            if(previewStyle) previewStyle.remove();
+
+            const previewScript = doc.getElementById('ai-preview-script');
+            if(previewScript) previewScript.remove();
+
+            //3. serialize clean HTML
+            const html = doc.documentElement.outerHTML;
+            return html
+        }
+    }))
 
     useEffect(()=>{
         const handleMessage = (event : MessageEvent)=> {
--- client/src/pages/Projects.tsx
+++ client/src/pages/Projects.tsx
@@ -34,7 +34,19 @@ const Projects = () => {
   }
 
   const downloadCode = ()=> {
-
+    const code = previewRef.current?.getCode() || project?.current_code;
+    if(!code){
+      if(isGenerating){
+        return
+      }
+      return
+    }
+    const element = document.createElement('a');
+    const file = new Blob([code],{type:'text/html'});
+    element.href = URL.createObjectURL(file);
+    element.download = 'indext.html';
+    document.body.appendChild(element);
+    element.click();
   }
 
   const togglePublish = async ()=> {
