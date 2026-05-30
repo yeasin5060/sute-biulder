4314f2f46dc5b64b1912d9d8f738d633c3723c16
Yeasin Munshi <142327064+yeasin5060@users.noreply.github.com>
2026-05-30 09:37:46 +0600

    add the cors option client url

--- server/server.ts
+++ server/server.ts
@@ -2,13 +2,21 @@ import "dotenv/config";
 import express from 'express';
 import cors from "cors";
 
+const corsOptions = {
+    origin: process.env.TRUSTED_ORIGIN?.split(',') || [],
+    credentials: true
+};
+
 const app = express();
 
+const port = process.env.PORT || 3000;
+
+
 // Middleware
-app.use(cors())
+app.use(cors(corsOptions))
 app.use(express.json());
 
-const port = process.env.PORT || 3000;
+
 
 app.get('/', (req, res) => {
     res.send('Server is Live!');
